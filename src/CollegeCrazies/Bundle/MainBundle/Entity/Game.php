8d5ac26187fb892dab5c8152f241a80ee7f6d1c4
Jonathon Suggs <jsuggs@murmp.com>
2011-12-10 15:12:43 -0600

    Moar model stuff

--- src/CollegeCrazies/Bundle/MainBundle/Entity/Game.php
+++ src/CollegeCrazies/Bundle/MainBundle/Entity/Game.php
@@ -14,12 +14,21 @@ use Doctrine\ORM\Mapping as ORM;
  * )
  */
 class Game {
-
     /**
      * @ORM\Id
-     * @ORM\Column
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="SEQUENCE")
+     * @ORM\SequenceGenerator(sequenceName="game_seq", initialValue=1, allocationSize=100)
      */
     protected $id;
+
+    /**
+     * The Bowl Game Name
+     * @ORM\Column(type="string", length="255")
+     *
+     * @var string
+     */
+    protected $name;
 
     /**
      * @ORM\OneToOne(targetEntity="Team")
@@ -32,11 +41,19 @@ class Game {
     protected $awayTeam;
 
     /**
+     * season
+     *
+     * @var string
+     */
+    protected $season;
+
+    /**
      * Gametime baby
      *
+     * @ORM\Column(type="datetime")
      * @var DateTime
      */
-    protected $date;
+    protected $gameDate;
 
     /**
      * Where the game is played
@@ -45,6 +62,14 @@ class Game {
      * @var string
      */
     protected $location;
+
+    /**
+     * network
+     *
+     * @ORM\Column(type="string", length="255")
+     * @var string
+     */
+    protected $network;
 
     public function setHomeTeam(Team $team)
     {
@@ -59,5 +84,35 @@ class Game {
     public function getAwayTeam()
     {
         return $this->awayTeam;
+    }
+
+    public function setAwayTeam(Team $team)
+    {
+        $this->awayTeam = $team;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getGameDate()
+    {
+        return $this->gameDate;
+    }
+
+    public function setNetwork($network)
+    {
+        $this->network = $network;
+    }
+
+    public function getNetwork()
+    {
+        return $this->network;
+    }
+
+    public function getLocation()
+    {
+        return $this->location;
     }
 }
--- src/CollegeCrazies/Bundle/MainBundle/Entity/Pick.php
+++ src/CollegeCrazies/Bundle/MainBundle/Entity/Pick.php
@@ -17,14 +17,32 @@ class Pick {
 
     /**
      * @ORM\Id
-     * @ORM\Column
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="SEQUENCE")
+     * @ORM\SequenceGenerator(sequenceName="pick_seq", initialValue=1, allocationSize=100)
      */
     protected $id;
 
+    /**
+     * @ORM\OneToOne(targetEntity="User")
+     */
     protected $user;
+
     protected $league;
+
+    /**
+     * @ORM\OneToOne(targetEntity="Game")
+     */
     protected $game;
+
+    /**
+     * @ORM\OneToOne(targetEntity="Team")
+     */
     protected $team;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
     protected $confidence;
 
     public function setTeam(Team $team)
@@ -33,5 +51,35 @@ class Pick {
             die('bad');
         }
         $this->team = $team;
+    }
+
+    public function setUser(User $user)
+    {
+        $this->user = $user;
+    }
+
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    public function setGame(Game $game)
+    {
+        $this->game = $game;
+    }
+
+    public function getGame()
+    {
+        return $this->game;
+    }
+
+    public function setConfidence($confidence)
+    {
+        $this->confidence = $confidence;
+    }
+
+    public function getConfidence()
+    {
+        return $this->confidence;
     }
 }
