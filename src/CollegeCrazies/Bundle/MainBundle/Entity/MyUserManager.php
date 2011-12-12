593ef0fd52e582a98e1c1dbe3c5f65b65697ded0
Jonathon Suggs <jsuggs@murmp.com>
2011-12-11 22:42:17 -0600

    Implement MyUserManaager to be able to login with either username or email address

---
+++ src/CollegeCrazies/Bundle/MainBundle/Entity/MyUserManager.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace CollegeCrazies\Bundle\MainBundle\Entity;
+
+use FOS\UserBundle\Entity\UserManager;
+use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
+
+class MyUserManager extends UserManager
+{
+    public function loadUserByUsername($username)
+    {
+        $user = $this->findUserByUsernameOrEmail($username);
+
+        if (!$user) {
+            throw new UsernameNotFoundException(sprintf('No user with name "%s" was found.', $username));
+        }
+
+        return $user;
+    }
+}
