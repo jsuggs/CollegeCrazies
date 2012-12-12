f4cc633289fcb8d673cb68fee414e232f9956936
Jonathon Suggs <jsuggs@murmp.com>
2012-12-11 20:45:58 -0600

    Update user profile links

--- src/CollegeCrazies/Bundle/UserBundle/Controller/ProfileController.php
+++ src/CollegeCrazies/Bundle/UserBundle/Controller/ProfileController.php
@@ -3,9 +3,11 @@
 namespace CollegeCrazies\Bundle\UserBundle\Controller;
 
 use FOS\UserBundle\Controller\ProfileController as BaseController;
+use FOS\UserBundle\Model\UserInterface;
 use JMS\SecurityExtraBundle\Annotation\Secure;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class ProfileController extends BaseController
 {
@@ -22,5 +24,30 @@ class ProfileController extends BaseController
         return array(
             'user' => $user,
         );
+    }
+
+    /**
+     * Edit the user
+     */
+    public function editAction()
+    {
+        $user = $this->container->get('security.context')->getToken()->getUser();
+        if (!is_object($user) || !$user instanceof UserInterface) {
+            throw new AccessDeniedException('This user does not have access to this section.');
+        }
+
+        $form = $this->container->get('fos_user.profile.form');
+        $formHandler = $this->container->get('fos_user.profile.form.handler');
+
+        $process = $formHandler->process($user);
+        if ($process) {
+            $this->setFlash('success', 'Profile Updated');
+
+            return new RedirectResponse($this->getRedirectionUrl($user));
+        }
+
+        return $this->container->get('templating')->renderResponse('FOSUserBundle:Profile:edit.html.twig', array(
+            'form' => $form->createView(),
+        ));
     }
 }
---
+++ src/CollegeCrazies/Bundle/UserBundle/Form/Type/ProfileFormType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace CollegeCrazies\Bundle\UserBundle\Form\Type;
+
+use Symfony\Component\Form\FormBuilderInterface;
+use FOS\UserBundle\Form\Type\ProfileFormType as BaseType;
+
+class ProfileFormType extends BaseType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        parent::buildForm($builder, $options);
+
+        // Additional Fields
+        $builder
+            ->add('emailVisible', 'checkbox', array(
+                'required' => false,
+                'label' => 'Email visible in leagues',
+            ))
+            ->add('firstName', 'text', array(
+                'required' => false,
+                'label' => 'First Name',
+            ))
+            ->add('lastName', 'text', array(
+                'required' => false,
+                'label' => 'Last Name',
+            ));
+    }
+
+    public function getName()
+    {
+        return 'college_crazies_user_profile';
+    }
+}
