93f9cc7a200d3fdd031a87c306174b57d41fdcbf
Jonathon Suggs <jsuggs@murmp.com>
2012-12-14 13:33:05 -0600

    Add a TextManipulator utility class and Twig extension

---
+++ src/CollegeCrazies/Bundle/MainBundle/Tests/Util/TextManipulatorTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace CollegeCrazies\Bundle\MainBundle\Tests\Util;
+
+use CollegeCrazies\Bundle\MainBundle\Util\TextManipulator;
+
+class TextManipulatorTest extends \PHPUnit_Framework_TestCase
+{
+    private $manipulator;
+
+    protected function setUp()
+    {
+        $this->manipulator = new TextManipulator();
+    }
+
+    /**
+     * @dataProvider testStrings
+     */
+    public function testTruncateText($text, $maxLen, $additionalText, $expectedResult)
+    {
+        if ($additionalText) {
+            $this->assertEquals($expectedResult, $this->manipulator->truncateText($text, $maxLen, $additionalText));
+        } else {
+            $this->assertEquals($expectedResult, $this->manipulator->truncateText($text, $maxLen));
+        }
+    }
+
+    public function testStrings()
+    {
+        return array(
+            array('test', 5, null, 'test'),
+            array('this is a test', 6, null, 'this is...'),
+            array('a ab abc abcd abcde abcdef', 15, null, 'a ab abc abcd...'),
+            array('test', 5, 'will not show up', 'test'),
+            array('this is a test', 6, ',,,', 'this is,,,'),
+        );
+    }
+}
