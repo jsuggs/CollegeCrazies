28c3f5a352b2266178fa34a0aadb02be6dd7f81b
Jonathon Suggs <jsuggs@murmp.com>
2013-02-11 21:11:42 -0600

    A home for a bracket

--- src/SofaChamps/Bundle/MarchMadnessBundle/Controller/AdminController.php
+++ src/SofaChamps/Bundle/MarchMadnessBundle/Controller/AdminController.php
@@ -2,8 +2,11 @@
 
 namespace SofaChamps\Bundle\MarchMadnessBundle\Controller;
 
+use JMS\SecurityExtraBundle\Annotation\Secure;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use SofaChamps\Bundle\MarchMadnessBundle\Entity\Bracket;
 
 /**
  * @Route("/admin")
@@ -20,6 +23,21 @@ class AdminController extends BaseController
 
         return array(
             'games' => $games,
+        );
+    }
+
+    /**
+     * @Route("/{year}/home", name="mm_admin_year")
+     * @ParamConverter("bracket", class="SofaChampsMarchMadnessBundle:Bracket", options={"id" = "year"})
+     * @Secure(roles="ROLE_ADMIN")
+     * @Template
+     */
+    public function homeAction(Bracket $bracket)
+    {
+        $request = $this->getRequest();
+
+        return array(
+            'bracket' => $bracket,
         );
     }
 }
