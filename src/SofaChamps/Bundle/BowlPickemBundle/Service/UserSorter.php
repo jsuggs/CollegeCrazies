07ad4e9b2878af5718f6e9c2529cb89109b3e5c6
Jonathon Suggs <jsuggs@murmp.com>
2013-09-27 17:54:00 -0500

    Namespace and configure UserSorter

--- src/SofaChamps/Bundle/BowlPickemBundle/Service/UserSorter.php
+++ src/SofaChamps/Bundle/BowlPickemBundle/Service/UserSorter.php
@@ -2,13 +2,24 @@
 
 namespace SofaChamps\Bundle\BowlPickemBundle\Service;
 
+use JMS\DiExtraBundle\Annotation as DI;
 use SofaChamps\Bundle\BowlPickemBundle\Entity\League;
 use SofaChamps\Bundle\CoreBundle\Entity\User;
 
+/**
+ * UserSorter
+ *
+ * @DI\Service("sofachamps.bp.user_sorter")
+ */
 class UserSorter
 {
     protected $pickSetSorter;
 
+    /**
+     * @DI\InjectParams({
+     *      "pickSetSorter" = @DI\Inject("sofachamps.bp.pickset_sorter")
+     * })
+     */
     public function __construct(PickSetSorter $pickSetSorter)
     {
         $this->pickSetSorter = $pickSetSorter;
