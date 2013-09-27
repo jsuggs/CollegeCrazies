9a797ff2427070843c9e5e0843fe06055e273d40
Jonathon Suggs <jsuggs@murmp.com>
2013-09-27 17:50:54 -0500

    Configure Twig extensions

--- src/SofaChamps/Bundle/BowlPickemBundle/Twig/Extension/AppExtension.php
+++ src/SofaChamps/Bundle/BowlPickemBundle/Twig/Extension/AppExtension.php
@@ -2,12 +2,24 @@
 
 namespace SofaChamps\Bundle\BowlPickemBundle\Twig\Extension;
 
+use JMS\DiExtraBundle\Annotation as DI;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
+/**
+ * AppExtension
+ *
+ * @DI\Service("sofachamps.bp.twig.app")
+ * @DI\Tag("twig.extension")
+ */
 class AppExtension extends \Twig_Extension
 {
     private $container;
 
+    /**
+     * @DI\InjectParams({
+     *      "container" = @DI\Inject("service_container")
+     * })
+     */
     public function __construct(ContainerInterface $container)
     {
         $this->container = $container;
@@ -27,6 +39,6 @@ class AppExtension extends \Twig_Extension
 
     public function getName()
     {
-        return 'sofachamps.app';
+        return 'sofachamps.bp.app';
     }
 }
--- src/SofaChamps/Bundle/BowlPickemBundle/Twig/Extension/TextExtension.php
+++ src/SofaChamps/Bundle/BowlPickemBundle/Twig/Extension/TextExtension.php
@@ -2,12 +2,24 @@
 
 namespace SofaChamps\Bundle\BowlPickemBundle\Twig\Extension;
 
+use JMS\DiExtraBundle\Annotation as DI;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
+/**
+ * TextExtension
+ *
+ * @DI\Service("sofachamps.bp.twig.text")
+ * @DI\Tag("twig.extension")
+ */
 class TextExtension extends \Twig_Extension
 {
     private $container;
 
+    /**
+     * @DI\InjectParams({
+     *      "container" = @DI\Inject("service_container")
+     * })
+     */
     public function __construct(ContainerInterface $container)
     {
         $this->container = $container;
@@ -27,6 +39,6 @@ class TextExtension extends \Twig_Extension
 
     public function getName()
     {
-        return 'sofachamps.text';
+        return 'sofachamps.bp.text';
     }
 }
