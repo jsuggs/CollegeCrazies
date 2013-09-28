078b36f19b4da76535595c4c8aadc0372248e71c
Jonathon Suggs <jsuggs@murmp.com>
2013-09-27 20:38:07 -0500

    Configure MyUserManager

--- src/SofaChamps/Bundle/CoreBundle/Entity/MyUserManager.php
+++ src/SofaChamps/Bundle/CoreBundle/Entity/MyUserManager.php
@@ -2,9 +2,15 @@
 
 namespace SofaChamps\Bundle\CoreBundle\Entity;
 
+use JMS\DiExtraBundle\Annotation as DI;
 use FOS\UserBundle\Doctrine\UserManager;
 use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
 
+/**
+ * MyUserManager
+ *
+ * @DI\Service("sofachamps.user.manager", public=false, parent="fos_user.user_manager.default")
+ */
 class MyUserManager extends UserManager
 {
     public function loadUserByUsername($username)
