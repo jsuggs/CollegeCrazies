95b23a4bc67c282a13b99c8d1dec2c86a1f65f79
Jonathon Suggs <jsuggs@murmp.com>
2013-12-03 16:40:37 -0600

    Better index names

--- app/DoctrineMigrations/Version20131203161117.php
+++ app/DoctrineMigrations/Version20131203161117.php
@@ -32,21 +32,21 @@ SQL;
         $this->addSql("CREATE SEQUENCE seq_expert_picks INCREMENT BY 1 MINVALUE 1 START 1");
         $this->addSql("CREATE SEQUENCE seq_experts INCREMENT BY 1 MINVALUE 1 START 1");
         $this->addSql(self::CREATE_BP_EXPERT_PICKS);
-        $this->addSql("CREATE INDEX IDX_5CB1286BE48FD905 ON bp_expert_picks (game_id)");
-        $this->addSql("CREATE INDEX IDX_5CB1286B296CD8AE ON bp_expert_picks (team_id)");
-        $this->addSql("CREATE INDEX IDX_5CB1286BC5568CE4 ON bp_expert_picks (expert_id)");
+        $this->addSql("CREATE INDEX IDX_BP_EXPERT_PICKS_GAME_ID ON bp_expert_picks (game_id)");
+        $this->addSql("CREATE INDEX IDX_BP_EXPERT_PICKS_TEAM_ID ON bp_expert_picks (team_id)");
+        $this->addSql("CREATE INDEX IDX_BP_EXPERT_PICKS_EXPERT_ID ON bp_expert_picks (expert_id)");
         $this->addSql(self::CREATE_BP_EXPERTS);
-        $this->addSql("ALTER TABLE bp_expert_picks ADD CONSTRAINT FK_5CB1286BE48FD905 FOREIGN KEY (game_id) REFERENCES games (id) NOT DEFERRABLE INITIALLY IMMEDIATE");
-        $this->addSql("ALTER TABLE bp_expert_picks ADD CONSTRAINT FK_5CB1286B296CD8AE FOREIGN KEY (team_id) REFERENCES teams (id) NOT DEFERRABLE INITIALLY IMMEDIATE");
-        $this->addSql("ALTER TABLE bp_expert_picks ADD CONSTRAINT FK_5CB1286BC5568CE4 FOREIGN KEY (expert_id) REFERENCES bp_experts (id) NOT DEFERRABLE INITIALLY IMMEDIATE");
+        $this->addSql("ALTER TABLE bp_expert_picks ADD CONSTRAINT FK_BP_EXPERT_PICKS_REFS_GAMES_GAME_ID FOREIGN KEY (game_id) REFERENCES games (id) NOT DEFERRABLE INITIALLY IMMEDIATE");
+        $this->addSql("ALTER TABLE bp_expert_picks ADD CONSTRAINT FK_BP_EXPERT_PICKS_REFS_TEAMS_TEAM_ID FOREIGN KEY (team_id) REFERENCES teams (id) NOT DEFERRABLE INITIALLY IMMEDIATE");
+        $this->addSql("ALTER TABLE bp_expert_picks ADD CONSTRAINT FK_BP_EXPERT_PICKS_REFS_BP_EXPERTS_EXPERT_ID FOREIGN KEY (expert_id) REFERENCES bp_experts (id) NOT DEFERRABLE INITIALLY IMMEDIATE");
     }
 
     public function down(Schema $schema)
     {
-        $this->addSql("ALTER TABLE bp_expert_picks DROP CONSTRAINT FK_5CB1286BC5568CE4");
-        $this->addSql("DROP SEQUENCE seq_expert_picks CASCADE");
-        $this->addSql("DROP SEQUENCE seq_experts CASCADE");
+        $this->addSql("ALTER TABLE bp_expert_picks DROP CONSTRAINT FK_BP_EXPERT_PICKS_REFS_BP_EXPERTS_EXPERT_ID");
         $this->addSql("DROP TABLE bp_expert_picks");
         $this->addSql("DROP TABLE bp_experts");
+        $this->addSql("DROP SEQUENCE seq_expert_picks CASCADE");
+        $this->addSql("DROP SEQUENCE seq_experts CASCADE");
     }
 }
