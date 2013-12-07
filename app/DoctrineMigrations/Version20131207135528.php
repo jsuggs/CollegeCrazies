9475cba48cca0ae2baf6dbeada4d7cde182d7a8d
Jonathon Suggs <jsuggs@murmp.com>
2013-12-07 16:18:01 -0600

    Better index names

--- app/DoctrineMigrations/Version20131207135528.php
+++ app/DoctrineMigrations/Version20131207135528.php
@@ -10,8 +10,8 @@ class Version20131207135528 extends AbstractMigration
     public function up(Schema $schema)
     {
         $this->addSql("ALTER TABLE users ADD referrer_id INT DEFAULT NULL");
-        $this->addSql("ALTER TABLE users ADD CONSTRAINT FK_1483A5E9798C22DB FOREIGN KEY (referrer_id) REFERENCES users (id) NOT DEFERRABLE INITIALLY IMMEDIATE");
-        $this->addSql("CREATE INDEX IDX_1483A5E9798C22DB ON users (referrer_id)");
+        $this->addSql("ALTER TABLE users ADD CONSTRAINT FK_USERS_REF_USERS_REFERRER_ID FOREIGN KEY (referrer_id) REFERENCES users (id) NOT DEFERRABLE INITIALLY IMMEDIATE");
+        $this->addSql("CREATE INDEX IDX_USERS_REFERRER_ID ON users (referrer_id)");
     }
 
     public function down(Schema $schema)
