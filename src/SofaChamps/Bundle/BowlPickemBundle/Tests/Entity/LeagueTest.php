4560e5074d1a14a392d49bcaf0fc18c04d9be627
Jonathon Suggs <jsuggs@murmp.com>
2013-12-12 14:57:16 -0600

    LeagueTest update for adding and removing users

--- src/SofaChamps/Bundle/BowlPickemBundle/Tests/Entity/LeagueTest.php
+++ src/SofaChamps/Bundle/BowlPickemBundle/Tests/Entity/LeagueTest.php
@@ -3,6 +3,7 @@
 namespace SofaChamps\Bundle\BowlPickemBundle\Tests\Service;
 
 use SofaChamps\Bundle\BowlPickemBundle\Entity\League;
+use SofaChamps\Bundle\CoreBundle\Entity\User;
 
 class LeagueTest extends \PHPUnit_Framework_TestCase
 {
@@ -14,5 +15,29 @@ class LeagueTest extends \PHPUnit_Framework_TestCase
         $this->assertFalse($league->isPublic());
         $league->setPassword(' ');
         $this->assertTrue($league->isPublic());
+    }
+
+    public function testAddRemoveUsers()
+    {
+        $league = new League();
+        $this->assertEquals(0, $league->getUsers()->count());
+
+        $user1 = new User();
+        $league->addUser($user1);
+        $this->assertEquals(1, $league->getUsers()->count());
+        $this->assertContains($user1, $league->getUsers());
+
+        $user2 = new User();
+        $league->addUser($user2);
+        $this->assertEquals(2, $league->getUsers()->count());
+        $this->assertContains($user2, $league->getUsers());
+
+        $league->addUser($user1);
+        $this->assertEquals(2, $league->getUsers()->count());
+
+        $league->removeUser($user1);
+        $this->assertEquals(1, $league->getUsers()->count());
+        $this->assertContains($user2, $league->getUsers());
+        $this->assertNotContains($user1, $league->getUsers());
     }
 }
