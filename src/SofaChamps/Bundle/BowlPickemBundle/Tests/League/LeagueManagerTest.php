582de49ca43287eb754e70c98c40d3d5e9f53ae1
Jonathon Suggs <jsuggs@murmp.com>
2013-12-12 15:05:09 -0600

    LeagueManagerTest

---
+++ src/SofaChamps/Bundle/BowlPickemBundle/Tests/League/LeagueManagerTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace SofaChamps\Bundle\BowlPickemBundle\Tests\League;
+
+use SofaChamps\Bundle\BowlPickemBundle\Entity\League;
+use SofaChamps\Bundle\BowlPickemBundle\Event\LeagueEvent;
+use SofaChamps\Bundle\BowlPickemBundle\Event\LeagueEvents;
+use SofaChamps\Bundle\BowlPickemBundle\League\LeagueManager;
+use SofaChamps\Bundle\CoreBundle\Tests\SofaChampsTest;
+
+class LeagueManagerTest extends SofaChampsTest
+{
+    protected $om;
+    protected $dispatcher;
+    protected $manager;
+
+    protected function setUp()
+    {
+        $this->om = $this->buildMock('Doctrine\Common\Persistence\ObjectManager');
+        $this->dispatcher = $this->buildMock('Symfony\Component\EventDispatcher\EventDispatcherInterface');
+
+        $this->manager = new LeagueManager($this->om, $this->dispatcher);
+    }
+
+    public function testCreateLeague()
+    {
+        $season = rand(2000, 3000);
+
+        $league = new League();
+        $league->setSeason($season);
+
+        $this->om->expects($this->once())
+            ->method('persist')
+            ->with($league);
+
+        $this->dispatcher->expects($this->once())
+            ->method('dispatch')
+            ->with(LeagueEvents::LEAGUE_CREATED, new LeagueEvent($league));
+
+        $this->manager->createLeague($season);
+    }
+}
+
