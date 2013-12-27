bcd6eed606b542ed3fff1d5ee04db15d7626af48
Jonathon Suggs <jsuggs@murmp.com>
2013-12-26 22:17:01 -0600

    List the conference members

---
+++ src/SofaChamps/Bundle/NCAABundle/Controller/ConferenceController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace SofaChamps\Bundle\NCAABundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use SofaChamps\Bundle\NCAABundle\Entity\Conference;
+
+/**
+ * @Route("/conference/{season}")
+ */
+class ConferenceController extends BaseController
+{
+    /**
+     * @Route("/{abbr}/members", name="ncaa_conference_members")
+     * @ParamConverter("conference", class="SofaChampsNCAABundle:Conference", options={"id" = "abbr"})
+     * @Template("SofaChampsNCAABundle:Conference:members.html.twig")
+     */
+    public function conferenceMembersAction(Conference $conference, $season)
+    {
+        return array(
+            'conference' => $conference,
+            'season' => $season,
+        );
+    }
+}
