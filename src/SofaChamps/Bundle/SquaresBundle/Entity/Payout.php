e0cd88e4038cb33b0a6065c91d9f52fc11c9daa8
Jonathon Suggs <jsuggs@murmp.com>
2014-01-13 23:23:12 -0600

    Trailing comma?

--- src/SofaChamps/Bundle/SquaresBundle/Entity/Payout.php
+++ src/SofaChamps/Bundle/SquaresBundle/Entity/Payout.php
@@ -11,7 +11,7 @@ use Symfony\Component\Validator\Constraints as Assert;
  *
  * @ORM\Entity
  * @ORM\Table(
- *      name="squares_game_payouts",
+ *      name="squares_game_payouts"
  * )
  */
 class Payout
