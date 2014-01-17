520eda7cbdfdbd5bfd895b0ba73e96602549f9fa
Jonathon Suggs <jsuggs@murmp.com>
2014-01-17 01:07:04 -0600

    EmailInputParser

---
+++ src/SofaChamps/Bundle/EmailBundle/Input/EmailInputParser.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace SofaChamps\Bundle\EmailBundle\Input;
+
+use JMS\DiExtraBundle\Annotation as DI;
+
+/**
+ * @DI\Service("sofachamps.email.input_guesser")
+ */
+class EmailInputParser
+{
+    public function parseEmails($text)
+    {
+        $emails = array();
+
+        $lines = explode("\n", $text);
+        foreach ($lines as $line) {
+            $cleaned = preg_replace('/\s+/', ',', $line);
+            $words = explode(',', $cleaned);
+            foreach ($words as $word) {
+                $emails[] = $word;
+            }
+        }
+
+        return array_unique(array_filter($emails, function ($email) {
+            return filter_var($email, FILTER_VALIDATE_EMAIL);
+        }));
+    }
+}
