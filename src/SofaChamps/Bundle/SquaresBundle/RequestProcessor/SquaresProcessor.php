6b9a42260619f67d81c9eccc29a1e0e1369ef1d3
Jonathon Suggs <jsuggs@murmp.com>
2014-01-28 00:39:42 -0600

    Processor for squares game

---
+++ src/SofaChamps/Bundle/SquaresBundle/RequestProcessor/SquaresProcessor.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace SofaChamps\Bundle\SquaresBundle\RequestProcessor;
+
+use Doctrine\Common\Persistence\ObjectManager;
+use JMS\DiExtraBundle\Annotation as DI;
+use SofaChamps\Bundle\CoreBundle\Entity\User;
+use SofaChamps\Bundle\SecurityBundle\RequestProcessor\RequestProcessor;
+use SofaChamps\Bundle\SquaresBundle\Game\InviteManager;
+use SofaChamps\Bundle\SquaresBundle\Game\PlayerManager;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Router;
+
+/**
+ * SquaresProcessor
+ *
+ * @DI\Service
+ * @DI\Tag("sofachamps.request_processor")
+ */
+class SquaresProcessor implements RequestProcessor
+{
+    /**
+     * @DI\InjectParams({
+     *      "router" = @DI\Inject("router"),
+     *      "om" = @DI\Inject("doctrine.orm.default_entity_manager"),
+     *      "playerManager" = @DI\Inject("sofachamps.squares.player_manager"),
+     * })
+     */
+    public function __construct(Router $router, ObjectManager $om, PlayerManager $playerManager)
+    {
+        $this->router = $router;
+        $this->om = $om;
+        $this->playerManager = $playerManager;
+    }
+
+    public function processRequest(Request $request, User $user)
+    {
+        if ($request->cookies->has(InviteManager::COOKIE_NAME)) {
+            $game = $this->om->getRepository('SofaChampsSquaresBundle:Game')->find($request->cookies->get(InviteManager::COOKIE_NAME));
+
+            if (!$game) {
+                return;
+            }
+
+            $player = $game->getPlayerForUser($user);
+
+            if (!$player) {
+                $this->playerManager->createPlayer($user, $game);
+                $this->om->flush();
+                $response = new RedirectResponse($this->router->generateUrl('squares_game_view', array(
+                    'gameId' => $game->getId(),
+                )));
+            }
+
+            if (isset($response)) {
+                return $response;
+            }
+        }
+    }
+}
+
+
