4e9bdb151481c687186b5c753abdd1866f3bb5a6
Jonathon Suggs <jsuggs@murmp.com>
2014-01-31 14:22:32 -0600

    Show login errors

--- src/SofaChamps/Bundle/UserBundle/Controller/RegistrationController.php
+++ src/SofaChamps/Bundle/UserBundle/Controller/RegistrationController.php
@@ -16,11 +16,8 @@ class RegistrationController extends BaseController
 {
     public function registerAction(Request $request)
     {
-        /** @var $formFactory \FOS\UserBundle\Form\Factory\FactoryInterface */
         $formFactory = $this->container->get('fos_user.registration.form.factory');
-        /** @var $userManager \FOS\UserBundle\Model\UserManagerInterface */
         $userManager = $this->container->get('fos_user.user_manager');
-        /** @var $dispatcher \Symfony\Component\EventDispatcher\EventDispatcherInterface */
         $dispatcher = $this->container->get('event_dispatcher');
 
         $user = $userManager->createUser();
